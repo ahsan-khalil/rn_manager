9cd14cd57a1a88f1e33bf5c834c5ac97efc82b60
Ahsan Khalil <ahsan.khalil@arbisoft.com>
2022-11-07 00:37:10 +0500

    boilerplate code done

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { Text, SafeAreaView } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
+import { initializeApp } from "firebase/app";
+
+class App extends Component {
+    componentDidMount() {
+        const firebaseConfig = {
+        apiKey: "AIzaSyChAwnE8YY7b_IYSK2zczhJYTifQF7g7jo",
+        authDomain: "rn-manager-f6345.firebaseapp.com",
+        projectId: "rn-manager-f6345",
+        storageBucket: "rn-manager-f6345.appspot.com",
+        messagingSenderId: "338643672059",
+        appId: "1:338643672059:web:328977f5f1f27c8135c95e"
+        };
+
+        // Initialize Firebase
+        const app = initializeApp(firebaseConfig);
+    }
+    render() {
+        return(
+            <Provider store={createStore(reducers)}>
+                <SafeAreaView>
+                    <Text>
+                        Hello!
+                    </Text>
+                </SafeAreaView>
+            </Provider>
+        );
+    }
+}
+
+export default App;
